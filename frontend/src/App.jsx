14d54c4b58209c5d8ad9c2902b18ce70d8091633
doruk.tekel <doruk.tekel@bilgeadam.com>
2024-02-26 00:01:39 +0300

    project was done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,10 +10,10 @@ const App = () => {
   return (
     <Routes>
       <Route exact path="/" element={<Home />} />
-      <Route exact path="/books/details/:id" element={<ShowBook />} />
-      <Route exact path="/books/create" element={<CreateBook />} />
-      <Route exact path="/books/edit/:id" element={<EditBook />} />
-      <Route exact path="/books/delete/:id" element={<DeleteBook />} />
+      <Route path="/books/details/:id" element={<ShowBook />} />
+      <Route path="/books/create" element={<CreateBook />} />
+      <Route path="/books/edit/:id" element={<EditBook />} />
+      <Route path="/books/delete/:id" element={<DeleteBook />} />
     </Routes>
   );
 };
--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import Spinner from "../components/Spinner";
+import BackButton from "../components/BackButton";
+import { useNavigate, useParams } from "react-router-dom";
+import { useSnackbar } from "notistack";
 
 const DeleteBook = () => {
-  return <div>DeleteBook</div>;
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const { enqueueSnackbar } = useSnackbar();
+
+  const handleDeleteBook = () => {
+    setLoading(true);
+    axios
+      .delete(`http://localhost:5555/books/${id}`)
+      .then(() => {
+        setLoading(false);
+        enqueueSnackbar("Book was deleted successfully", {
+          variant: "success",
+        });
+        navigate("/");
+      })
+      .catch((err) => {
+        console.log(err);
+        enqueueSnackbar("Book was deleted successfully", {
+          variant: "error",
+        });
+        setLoading(false);
+      });
+  };
+
+  return (
+    <div className="p-4">
+      <BackButton />
+      <h1 className="my-4 text-3xl">Delete Book</h1>
+      {loading && <Spinner />}
+      <div className="flex flex-col items-center  border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto">
+        <h3 className="text-2xl">
+          Are you sure you want to delete this book ?
+        </h3>
+        <button
+          className="p-4 bg-red-600 text-white m-8 w-full"
+          onClick={handleDeleteBook}
+        >
+          Yes delete it !
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default DeleteBook;
